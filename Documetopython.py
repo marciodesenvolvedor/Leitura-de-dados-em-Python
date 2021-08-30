15a6fc5334298b8acbabeea3d387238031b7e6c8
marciodesenvolvedor <89565704+marciodesenvolvedor@users.noreply.github.com>
2021-08-30 15:42:31 -0300

    Programa simples de Leitura de dados em Python
    Lendo o famoso Olá Mundo com dados pessoais em Python

---
+++ Documetopython.py
@@ -0,0 +1,8 @@
+﻿print("Ola mundo")
+Olá mundo
+
+nome = "Marcio"
+sobrenome = "Pereira"
+idade = 42
+print("Olá amigo,", nome, sobrenome, "Sua idade é", idade)
+Olá amigo, Marcio Pereira Sua idade é 42
